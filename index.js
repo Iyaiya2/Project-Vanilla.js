f068202d4607e3c07341196237dad3e66cf74005
Iyaiya2 <iyad.hadjour@hotmail.com>
2024-11-15 12:07:16 +0100

    edit js et html

--- index.js
+++ index.js
@@ -1,4 +1,11 @@
-document.getElementById("button-addd-todo")
+document.getElementById("button-add-todo")
 addEventListener("click", function (){
     console.log("click");
 });
+
+let todoList = document.getElementById("todo-list");
+
+todoList.innerHTML = "<li>Une Todo !</li>";
+todoList.innerHTML += "<li>Une autre todo , list.. </li>";
+todoList.innerHTML += "<li> Bon vasi j'ai faim  </li>";
+
